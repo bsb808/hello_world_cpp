69ef03e5fe1eed884e6b38d7cf34588dd71699a1
Bingham, Brian S <bsbingha@nps.edu>
2017-02-03 14:56:00 -0800

    adding doxygen bits

---
+++ include/hello_world_cpp.h
@@ -0,0 +1,23 @@
+/**
+* @file hello_world_cpp.h
+* @brief C++ source file
+*
+* @author Brian Bingham
+*
+* @date 2017
+*/
+
+#ifndef _HELLO_H
+#define _HELLO_H
+
+/** @brief Prints Hello World!
+ *
+ * Yes, it prints Hello World!
+ * @return 0
+ * @param argc argc
+ * @param argv argv
+ * @todo Not much
+ */
+int main(int argc, char *argv[]);
+
+#endif
--- src/hello_world_cpp.cpp
+++ src/hello_world_cpp.cpp
@@ -1,4 +1,14 @@
+/**
+* @file hello_world_cpp.cpp
+* @brief C++ source file
+*
+* @author Brian Bingham
+*
+* @date 2017
+*/
+
 #include<iostream>
+#include "hello_world_cpp.h"
 
 int main(int argc, char *argv[]){
    std::cout << "Hello World!" << std::endl;
